7b07dcdce4668f0fad33dd5a29f453dfddef908f
Marciano Schildmeijer <marciano@kabisa.nl>
2022-01-30 16:32:11 +0100

    Refactor test.

--- components/ExcersiseInfoItem/ExcersiseInfoItem.spec.tsx
+++ components/ExcersiseInfoItem/ExcersiseInfoItem.spec.tsx
@@ -4,12 +4,18 @@ import ExcersiseInfoItem from "./";
 describe("ExcersiseInfoItem", () => {
 	it("renders the ExcersiseInfoItem with it's excersise name, sets and reps", () => {
 		const { getByText } = render(
-			<ExcersiseInfoItem excersise="Low bar squat" sets="4" reps="4" />
+			<ExcersiseInfoItem
+				excersise="Low bar squat"
+				sets="4"
+				reps="4"
+				lastTimeLifts={{ RPE: "10", lift: "300kg x 1" }}
+			/>
 		);
 
 		getByText("Oefening");
 		getByText("Low bar squat");
 		getByText("Sets");
 		getByText("Reps");
+		getByText("Last time");
 	});
 });
