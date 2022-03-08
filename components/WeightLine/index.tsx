65195458a9f0785fc3fe6bb886db783326c4af63
Marciano Schildmeijer <marciano@kabisa.nl>
2022-03-08 21:56:24 +0100

    Responsive "reps" text.

--- components/WeightLine/index.tsx
+++ components/WeightLine/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, CSSProperties } from "react";
 import styles from "./styles.module.scss";
 
 type WeightLineProps = {
@@ -6,13 +6,26 @@ type WeightLineProps = {
 	reps: number;
 };
 
+interface FontSize extends CSSProperties {
+	"--textLength": string;
+}
+
+// Check if this is possible with CSS Container queries
+
 const WeightLine: FC<WeightLineProps> = ({ weight, reps }) => {
+	const size = reps.toString().length < 4 ? 2.5 : 2;
+
 	return (
 		<div className={styles.weightContainer}>
 			<span className={styles.weight}>{weight}</span>
 			<span className={styles.kg}>kg</span>{" "}
 			<span className={styles.x}>x</span>{" "}
-			<span className={styles.reps}>{reps}</span>
+			<span
+				className={styles.reps}
+				style={{ "--textLength": `${size}rem` } as FontSize}
+			>
+				{reps}
+			</span>
 		</div>
 	);
 };
