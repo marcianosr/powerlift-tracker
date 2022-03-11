0896e78e0652cef94aaf24c9dd7ebef192df5762
Marciano Schildmeijer <marciano@kabisa.nl>
2022-03-11 16:48:54 +0100

    Add container query polyfill.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,13 @@ import "@/styles/global.scss";
 import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+	if (typeof window !== "undefined") {
+		const supportsContainerQueries =
+			"container" in document.documentElement.style;
+
+		if (!supportsContainerQueries) {
+			import("container-query-polyfill");
+		}
+	}
 	return <Component {...pageProps} />;
 }
