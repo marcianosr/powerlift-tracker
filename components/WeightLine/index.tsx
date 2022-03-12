2aa84eaf26fa5c7415f44e08ab6f0b89be990e2a
Marciano Schildmeijer <marciano@kabisa.nl>
2022-03-12 22:45:28 +0100

    Add long weight text.

--- components/WeightLine/index.tsx
+++ components/WeightLine/index.tsx
@@ -1,8 +1,9 @@
+import classNames from "classnames";
 import React, { FC, CSSProperties } from "react";
 import styles from "./styles.module.scss";
 
 type WeightLineProps = {
-	weight: number;
+	weight: number | string;
 	reps: number;
 };
 
@@ -14,22 +15,60 @@ interface FontSize extends CSSProperties {
 
 const WeightLine: FC<WeightLineProps> = ({ weight, reps }) => {
 	const size = reps.toString().length < 4 ? 2.5 : 2;
-	console.log(weight);
+	const weightTextIsTooLong = typeof weight === "string"; // strings are too long
+	const hasAddedWeight = weightTextIsTooLong && weight.split("+")[0]; // when microloading to machines
 
 	return (
-		<div className={styles.weightContainer}>
-			<span className={styles.weight}>{weight}</span>
-			{typeof weight === "number" && (
-				<span className={styles.kg}>kg</span>
-			)}{" "}
-			<span className={styles.x}>x</span>{" "}
-			<span
-				className={styles.reps}
-				style={{ "--textLength": `${size}rem` } as FontSize}
-			>
-				{reps}
-			</span>
-		</div>
+		<>
+			{weightTextIsTooLong ? (
+				<div
+					className={classNames(styles.weightContainer, styles.long)}
+				>
+					<div>
+						{hasAddedWeight && (
+							<>
+								<span
+									style={{ fontSize: "1.85rem" }}
+									className={styles.weight}
+								>
+									{hasAddedWeight}
+								</span>
+								<span style={{ fontSize: "1.25rem" whiteSpace: "nowrap" }}>
+									+ {weight.split("+")[1]}
+								</span>
+								<span className={styles.x}>x</span>{" "}
+							</>
+						)}
+						{!hasAddedWeight && (
+							<>
+								<span className={styles.weight}>
+									{weight.split("+")[0]}
+								</span>
+								<span className={styles.x}>x</span>{" "}
+							</>
+						)}
+					</div>
+					<span
+						className={styles.reps}
+						style={{ "--textLength": `${size}rem` } as FontSize}
+					>
+						{reps}
+					</span>
+				</div>
+			) : (
+				<div className={classNames(styles.weightContainer)}>
+					<span className={styles.weight}>{weight}</span>
+					<span className={styles.kg}>kg</span>{" "}
+					<span className={styles.x}>x</span>{" "}
+					<span
+						className={styles.reps}
+						style={{ "--textLength": `${size}rem` } as FontSize}
+					>
+						{reps}
+					</span>
+				</div>
+			)}
+		</>
 	);
 };
 
