384c7ec44e6cdd99d027f7d0baa53d5f364e5fe5
Marciano Schildmeijer <marciano@kabisa.nl>
2022-03-22 13:01:27 +0100

    Move firebase init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import "@/styles/global.scss";
 import type { AppProps } from "next/app";
+import { initializeFirebase } from "util/firebase";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+	initializeFirebase();
+
 	if (typeof window !== "undefined") {
 		const supportsContainerQueries =
 			"container" in document.documentElement.style;
